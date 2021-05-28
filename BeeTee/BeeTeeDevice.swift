cde2735a143167d3727ba47c32151a06a131084c
Martin Rait <fspacerpg@users.noreply.github.com>
2021-05-29 11:59:31 +1200

    Initial upgrade work - and commsUpgrade comments and config changes for upgrade

--- BeeTee/BeeTeeDevice.swift
+++ BeeTee/BeeTeeDevice.swift
@@ -1,6 +1,8 @@
 /*
  This file is part of BeeTee Project. It is subject to the license terms in the LICENSE file found in the top-level directory of this distribution and at https://github.com/michaeldorner/BeeTee/blob/master/LICENSE. No part of BeeTee Project, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the LICENSE file.
  */
+// This project has been modified by Martin Rait in 2021 to comply with latest
+// Apple Swift version and XCode requirements.
 
 import Foundation
 
